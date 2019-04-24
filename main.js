c1e567bf2e476530aea7c7b46a1e58500493048b
Callam Farrell <calfazzo@hotmail.com>
2019-04-24 18:55:20 -0400

    changed all the stuff

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 // The $ sign is JQuery, that is a javascript library, it helps you do things that would be harder with pure javascript
 $("#toggle-button").click(function() {
-  $("#optional-text").toggle();
+  $("#optional-text").toggle(100);
 });
